8b743e331665d729144bc0b251cfc268a23d15eb
haihung01 <104615629+haihung01@users.noreply.github.com>
2023-05-17 21:08:12 +0700

    img

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Cart = () => {
     return (
         <div>
-
+            <img src='https://i.pinimg.com/originals/48/68/fb/4868fbbf48c977bc109b93f3ab92ff16.png' />
+            <h1 style={{ textAlign: 'center' }}>Cái này cũng chưa làm tới luôn ~.~ </h1>
         </div>
     );
 };
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Shop = () => {
     return (
         <div>
-
+            <img src='https://play-lh.googleusercontent.com/FfwD12bTR1qvgnN7uUl9peanNII1WmCbK1FA523OUJQfmnSjXGaYVXaWDPoxDC-hHg' />
+            <h1 style={{ textAlign: 'center' }}>Chưa làm tới mừ ~.~ </h1>
         </div>
     );
 };
